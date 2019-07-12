9b046eef9555232e2e470bf1b35db609bc290b95
Shimai1122 <52818181+Shimai1122@users.noreply.github.com>
2019-07-12 17:25:56 +0800

    Add files via upload

--- cachesolve.R
+++ cachesolve.R
@@ -1,3 +1,7 @@
+#solve the inverse of a matrix using "cache"
+
+#Return a special list, containing function to set the value of matrix; get the value of matrix; 
+#set the inverse of matrix; and get the inverse of matrix, respectively
 makeMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,6 +16,8 @@ makeMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
+#Solve the inverse of a matrix created above. If the inverse has already been calculated, get it.
+#If it is a new matrix, calculate the inverse and save the result into the cache.
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
